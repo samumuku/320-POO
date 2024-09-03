5aac974c4d20cfd12db49991e1554675204e2c51
samumuku <sallakusamuel39@gmail.com>
2024-09-03 13:13:32 +0200

    snail

---
+++ personnel/snail/snail/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snail
+{
+    internal class Snail
+    {
+        public string _Snail;
+        public int _HPsnail;
+        public int _PosX;
+        public int _PosY;
+
+
+        public Snail(string Corps, int VieEsc)
+        {
+            this._Snail = Corps;
+            this._HPsnail = VieEsc;
+        }
+
+        public void Move()
+        {
+            _PosX++;
+            _HPsnail--;
+        }
+        public void Hide()
+        {
+            Console.Clear();
+        }
+        public bool IsAlive()
+        {
+            if(_HPsnail > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Show()
+        {
+            Console.SetCursorPosition(_PosX,_PosY);
+        }
+        public override string ToString()
+        {
+            return _Snail;
+        }
+        
+        static void Main(string[] args)
+        {
+            Snail snail1 = new Snail("_@_ö", 50);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.CursorVisible = false;
+
+            while (snail1.IsAlive())
+            {   
+
+                Console.WriteLine(snail1.ToString());
+                snail1.Move();
+                Thread.Sleep(100);
+                snail1.Hide();
+                snail1.Show();
+            }
+
+        }
+    }
+}
