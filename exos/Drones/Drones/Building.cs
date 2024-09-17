24330cb3839f76435ab6b4b38be1e6fca8dea622
samumuku <sallakusamuel39@gmail.com>
2024-09-17 15:38:32 +0200

    exos

---
+++ exos/Drones/Drones/Building.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones
+{
+    internal class Building
+    {
+        private int PosX;
+        private int PosY;
+        private 
+
+
+    }
+}
